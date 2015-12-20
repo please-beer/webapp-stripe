dd84734b9f8c2c16ca7d0e47910467ceac6f4298
Dariusz Tarczynski <dariusz.tarczynski@gmail.com>
2015-12-20 20:12:38 +0100

    changed aadd card method

--- app/lib/add-card.js
+++ app/lib/add-card.js
@@ -1,15 +1,18 @@
 var BPromise = require("bluebird");
 var MW       = require("meteor-wapi");
 
-module.exports = function addCard (cardToken) {
+module.exports = function addCard (params) {
     return BPromise.bind(this)
         /*
-        *   Adds the card to the user.
+        *   Adds the card to the user. exp_month,exp_year,number,cardholder_name
         *   Stripe API reference: https://stripe.com/docs/api#create_card
         */
         .then(function () {
             return this.stripe.customers.createSource(this.user.stripeCustomer.id, {
-                source: cardToken
+                source: {object: "card",
+                exp_month: params.exp_month,
+                exp_year: params.exp_year,
+                number: params.number}
             });
         })
         /* Normalize errors */
@@ -18,6 +21,6 @@ module.exports = function addCard (cardToken) {
             *   TODO: we might want to give feedback on what precisely went
             *   wrong.
             */
-            throw new MW.Error(400, "Error while adding the card.");
+            throw new MW.Error(400, "Error while adding the card." + err);
         });
 };
